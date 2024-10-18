f49e183ed80318d8c005ffac01a28b92ea85a098
Omindu Hirushka <ominduhirushka7@gmail.com>
2024-10-19 04:51:53 +0530

    user login/signup and jwt

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
 import bodyParser from "body-parser";
 import express from "express";
+import mongoose from "mongoose";
+import userRouter from "./src/routes/UserRoutes.js";
+import jwt from "jsonwebtoken";
 
 const app = express();
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  const name = "VILLA PARK";
-  const message = "Welcome to " + name + " Hotel";
+app.use((req, res, next) => {
+  const token = req.header("Authorization")?.replace("Bearer ", "");
 
-  res.json({
-    message: message,
-  });
-  
-  console.log(message);
+  if (token != null) {
+    jwt.verify(token, "secret", (err, decoded) => {
+      if (decoded != null) {
+        req.user = decoded;
+        next();
+      } else {
+        next();
+      }
+    });
+  } else {
+    next();
+  }
 });
+
+const connectionString =
+  "mongodb+srv://tester:1234@cluster0.zhsq8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose
+  .connect(connectionString)
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch(() => {
+    console.log("Connection failed");
+  });
+
+app.use("/api/users", userRouter);
 
 app.listen(5000, (req, res) => {
   console.log("Server is running on port 5000");
---
+++ src/controllers/UserController.js
@@ -0,0 +1,64 @@
+import User from "../models/user.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+export function createUser(req, res) {
+  const user = req.body;
+
+  const password = req.body.password;
+  const passwordHash = bcrypt.hashSync(password, 10);
+
+  user.password = passwordHash;
+
+  const newUser = new User(user);
+  newUser
+    .save()
+    .then((savedUser) => {
+      res.status(201).json({
+        message: "User created successfully",
+        user: savedUser,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "User creation failed",
+        error: err.message,
+      });
+    });
+}
+
+export function loginUser(req, res) {
+  const login = req.body;
+
+  User.findOne({ email: login.email }).then((user) => {
+    if (!user) {
+      res.status(403).json({
+        message: "User not found",
+      });
+    } else {
+      const isPasswordValid = bcrypt.compareSync(login.password, user.password);
+
+      if (!isPasswordValid) {
+        res.status(403).json({
+          message: "Incorrect password",
+        });
+      } else {
+        const payload = {
+          id: user._id,
+          email: user.email,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          type: user.type,
+        };
+
+        const token = jwt.sign(payload, "secret", { expiresIn: "1h" });
+
+        res.json({
+          message: "User found",
+          user: user,
+          token: token,
+        });
+      }
+    }
+  });
+}
