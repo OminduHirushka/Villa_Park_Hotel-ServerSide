76d84cf690500db8a8c72a19161c891061f9df46
Omindu Hirushka <ominduhirushka7@gmail.com>
2024-11-23 22:29:54 +0530

    update gallery item controller

--- src/controllers/GalleryItemController.js
+++ src/controllers/GalleryItemController.js
@@ -1,4 +1,5 @@
 import GalleryItem from "../models/GalleryItem.js";
+import { isAdminValid } from "./UserController.js";
 
 export function createGalleryItem(req, res) {
   const user = req.user;
@@ -41,4 +42,69 @@ export function getGalleryItem(req, res) {
       list: list,
     });
   });
+}
+
+export function updateGalleryItem(req, res) {
+  if (!isAdminValid(req)) {
+    res.status(403).json({
+      message: "Not Authorized",
+    });
+    return;
+  }
+
+  const galleryItemName = req.params.name;
+  const updatedGallery = req.body;
+
+  GalleryItem.findOneAndUpdate({ name: galleryItemName }, updatedGallery)
+    .then((result) => {
+      if (!result) {
+        res.status(404).json({
+          message: "Gallery Item not found",
+          name: galleryItemName,
+        });
+      } else {
+        res.status(201).json({
+          message: "Successfull",
+          result: updatedGallery,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Failed",
+        error: err,
+      });
+    });
+}
+
+export function deleteGalleryItem(req, res) {
+  if (!isAdminValid(req)) {
+    res.status(403).json({
+      message: "Not Authorized",
+    });
+    return;
+  }
+
+  const galleryItemName = req.params.name;
+
+  GalleryItem.findOneAndDelete({ name: galleryItemName })
+    .then((result) => {
+      if (!result) {
+        res.status(404).json({
+          message: "Gallery Item not found",
+          name: galleryItemName,
+        });
+      } else {
+        res.status(201).json({
+          message: "Gallery Item deleted successfully",
+          name: galleryItemName,
+        });
+      }
+    })
+    .catch((err) => {
+      res.json({
+        message: "Gallery Item deletion failed",
+        error: err,
+      });
+    });
 }
--- src/routes/GlleryItemeRoutes.js
+++ src/routes/GlleryItemeRoutes.js
@@ -2,11 +2,15 @@ import express from "express";
 import {
   createGalleryItem,
   getGalleryItem,
+  updateGalleryItem,
+  deleteGalleryItem,
 } from "../controllers/GalleryItemController.js";
 
 const galleryRouter = express.Router();
 
 galleryRouter.post("/", createGalleryItem);
 galleryRouter.get("/", getGalleryItem);
+galleryRouter.put("/:name", updateGalleryItem);
+galleryRouter.delete("/:name", deleteGalleryItem);
 
 export default galleryRouter;
