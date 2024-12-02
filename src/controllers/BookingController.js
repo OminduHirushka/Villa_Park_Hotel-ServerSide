ec5b173d0120e9db8a3e8440670caaa86762d408
Omindu Hirushka <ominduhirushka7@gmail.com>
2024-12-03 01:04:07 +0530

    update booking controller

--- src/controllers/BookingController.js
+++ src/controllers/BookingController.js
@@ -1,5 +1,5 @@
 import Booking from "../models/Booking.js";
-import { isCustomerValid } from "./UserController.js";
+import { isCustomerValid, isAdminValid } from "./UserController.js";
 
 export function createBooking(req, res) {
   if (!isCustomerValid(req)) {
@@ -41,6 +41,130 @@ export function createBooking(req, res) {
     .catch((err) => {
       res.json({
         message: "Booking creation failed",
+        error: err,
+      });
+    });
+}
+
+export function getBooking(req, res) {
+  Booking.find().then((list) => {
+    res.json({
+      message: "Successfull",
+      list: list,
+    });
+  });
+}
+
+export function getBookingById(req, res) {
+  const id = req.params.bookingId;
+
+  Booking.find({ bookingId: id })
+    .then((result) => {
+      if (!result) {
+        res.status(404).json({
+          message: "Booking not found",
+          bookingId: id,
+        });
+      } else {
+        res.status(201).json({
+          message: "Successfull",
+          result: result,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Failed",
+        error: err,
+      });
+    });
+}
+
+export function getBookingByRoomId(req, res) {
+  const id = req.params.roomId;
+
+  Booking.find({ roomId: id })
+    .then((result) => {
+      if (!result) {
+        res.status(404).json({
+          message: "Booking not found",
+          roomId: id,
+        });
+      } else {
+        res.status(201).json({
+          message: "Successfull",
+          result: result,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Failed",
+        error: err,
+      });
+    });
+}
+
+export function updateBooking(req, res) {
+  if (!isCustomerValid(req) && !isAdminValid(req)) {
+    res.status(403).json({
+      message: "Not Authorized",
+    });
+    return;
+  }
+
+  const id = req.params.bookingId;
+  const updatedBooking = req.body;
+
+  Booking.findOneAndUpdate({ bookingId: id }, updatedBooking)
+    .then((result) => {
+      if (!result) {
+        res.status(404).json({
+          message: "Booking not found",
+          bookingId: id,
+        });
+      } else {
+        res.status(201).json({
+          message: "Successfull",
+          result: updatedBooking,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Failed",
+        error: err,
+      });
+    });
+}
+
+export function deleteBooking(req, res) {
+  if (!isCustomerValid(req) && !isAdminValid(req)) {
+    res.status(403).json({
+      message: "Not Authorized",
+    });
+    return;
+  }
+
+  const id = req.params.bookingId;
+
+  Booking.findOneAndDelete({ bookingId: id })
+    .then((result) => {
+      if (!result) {
+        res.status(404).json({
+          message: "Booking not found",
+          bookingId: id,
+        });
+      } else {
+        res.status(201).json({
+          message: "Booking deleted successfully",
+          result: id,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Booking deletion failed",
         error: err,
       });
     });
--- src/routes/BookingRoutes.js
+++ src/routes/BookingRoutes.js
@@ -1,8 +1,20 @@
 import express from "express";
-import { createBooking } from "../controllers/BookingController.js";
+import {
+  createBooking,
+  getBooking,
+  updateBooking,
+  deleteBooking,
+  getBookingById,
+  getBookingByRoomId,
+} from "../controllers/BookingController.js";
 
 const bookingRouter = express.Router();
 
 bookingRouter.post("/", createBooking);
+bookingRouter.get("/", getBooking);
+bookingRouter.put("/:bookingId", updateBooking);
+bookingRouter.delete("/:bookingId", deleteBooking);
+bookingRouter.get("/:bookingId", getBookingById);
+bookingRouter.get("/room/:roomId", getBookingByRoomId);
 
 export default bookingRouter;
